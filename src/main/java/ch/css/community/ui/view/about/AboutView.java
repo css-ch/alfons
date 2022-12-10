88ba511b5c9a197aee1de9fac092fb5ca4c78d7a
Marcus Fihlon <marcus@fihlon.swiss>
2022-12-10 12:38:35 +0100

    🛂 No anonymous access

--- src/main/java/ch/css/community/ui/view/about/AboutView.java
+++ src/main/java/ch/css/community/ui/view/about/AboutView.java
@@ -8,12 +8,13 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
-import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+import javax.annotation.security.PermitAll;
 
 @PageTitle("About")
 @Route(value = "about", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
-@AnonymousAllowed
+@PermitAll
 public class AboutView extends VerticalLayout {
 
     public AboutView() {
